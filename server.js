b9324cc0a4d0f8a24048d46bd3b9d4bded88b6e3
Robbie Page <robbie.page@e-gineering.com>
2019-04-07 21:03:05 -0400

    switch to async and await

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var os = require("os");
-const redis = require("redis");
+const redis = require("async-redis");
 const fetch = require("node-fetch");
 
 var hostname = os.hostname();
@@ -21,96 +21,56 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.get("/api/wdw", (req, resp) => {
-    var request = {
-        lat: 28.4160036778,
-        lon: -81.5811902834,
-        key: 'wdw'
-    };
-
-    getWeatherInformation(request, resp);
-    return;
+app.get("/api/wdw", async (req, resp) => {
+    var result = await getWeatherInformation(28.4160036778, -81.5811902834, 'wdw');
+    resp.json(result);
 });
 
-app.get("/api/dlr", (req, resp) => {
-    var request = {
-        lat: 33.8095545068,
-        lon: -117.9189529669,
-        key: 'dlr'
-    };
-
-    getWeatherInformation(request, resp);
-    return;
+app.get("/api/dlr", async (req, resp) => {
+    var result = await getWeatherInformation(33.8095545068, -117.9189529669, 'dlr');
+    resp.json(result);
 });
 
-app.get("/api/tdr", (req, resp) => {
-    var request = {
-        lat: 35.6329,
-        lon: 139.8804,
-        key: 'tdr'
-    };
-
-    getWeatherInformation(request, resp);
-    return;
+app.get("/api/tdr", async (req, resp) => {
+    var result = await getWeatherInformation(35.6329, 139.8804, 'tdr');
+    resp.json(result);
 });
 
-app.get("/api/dlp", (req, resp) => {
-    var request = {
-        lat: 48.870205,
-        lon: 2.779913,
-        key: 'dlp'
-    };
-
-    getWeatherInformation(request, resp);
-    return;
+app.get("/api/dlp", async (req, resp) => {
+    var result = await getWeatherInformation(48.870205, 2.779913, 'dlp');
+    resp.json(result);
 });
 
-app.get("/api/hkdl", (req, resp) => {
-    var request = {
-        lat: 22.313131,
-        lon: 114.044517,
-        key: 'hkdl'
-    };
-
-    getWeatherInformation(request, resp);
-    return;
+app.get("/api/hkdl", async (req, resp) => {
+    var result = await getWeatherInformation(22.313131, 114.044517, 'hkdl');
+    resp.json(result);
 });
 
-app.get("/api/shdr", (req, resp) => {
-    var request = {
-        lat: 31.147097966725,
-        lon: 121.66901898194,
-        key: 'shdr'
-    };
-
-    getWeatherInformation(request, resp);
-    return;
+app.get("/api/shdr", async (req, resp) => {
+    var result = await getWeatherInformation(31.147097966725, 121.66901898194, 'shdr');
+    resp.json(result);
 });
 
-function getWeatherInformation(req, resp) {
-    redis_client.get(req.key, (err, result) => {
-        if (result != null) {
-            resp.json(JSON.parse(result));
-        } else {
-            fetch(
-                'https://api.darksky.net/forecast/' +
-                    weatherApiKey +
-                    '/' +
-                    req.lat +
-                    ',' +
-                    req.lon
-            )
-            .then(res => res.json())
-            .then(json => {
-                redis_client.setex(req.key, weather_time_to_live, JSON.stringify(json));
-                resp.json(json);
-            })
-            .catch(err => {
-                console.error(err);
-                resp.send(202);
-            });
-        }
-    });
+async function getWeatherInformation(lat, lon, key) {
+    let redis_data = await redis_client.get(key);
+
+    if (redis_data === null) {
+        let darkSkyDataRequest = await fetch(
+            'https://api.darksky.net/forecast/' +
+                weatherApiKey +
+                '/' +
+                lat +
+                ',' +
+                lon +
+                '?exclude=minutely,hourly,alerts,flags'
+        );
+
+        let responseJson = await darkSkyDataRequest.json();
+        await redis_client.setex(key, weather_time_to_live, JSON.stringify(responseJson));
+        return responseJson;
+    } else {
+        return JSON.parse(redis_data);
+    }
 }
 
 app.listen(port);
